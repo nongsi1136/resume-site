9a11293b22c18e9339a14758e9a65e948fc454ec
sinhong jeong <jsh719803@gmail.com>
2024-02-02 15:36:41 +0900

    create Resume delete API

--- routers/documents.router.js
+++ routers/documents.router.js
@@ -49,31 +49,36 @@ router.get("/resumes", async (req, res, next) => {
 router.get("/resumes/:userId", async (req, res, next) => {
   const { userId } = req.params;
 
-  const resume = await prisma.resumes.findUnique({
-    where: { resumeId: parseInt(userId) },
-    select: {
-      resumeId: true,
-      title: true,
-      content: true,
-      user: {
-        select: { name: true },
+  try {
+    const resume = await prisma.resumes.findUnique({
+      where: { resumeId: parseInt(userId) },
+      select: {
+        resumeId: true,
+        title: true,
+        content: true,
+        user: {
+          select: { name: true },
+        },
+        createdAt: true,
+        status: true,
       },
-      createdAt: true,
-      status: true,
-    },
-  });
+    });
 
-  if (!resume) {
-    return res.status(404).json({ message: "이력서를 찾을 수 없습니다." });
+    if (!resume) {
+      return res.status(404).json({ message: "이력서를 찾을 수 없습니다." });
+    }
+    return res.status(200).json({ data: resume });
+  } catch (error) {
+    next(error);
   }
 });
 
 //이력서 수정 API 비즈니스 로직
-router.post("/resumes/:userId", async (req, res, next) => {
+router.post("/resumes/:resumeId", async (req, res, next) => {
   // 해당하는 이력서를 조회해야 겠징
   try {
     const resume = await prisma.resumes.findUnique({
-      where: { userId: parseInt(userId) },
+      where: { resumeId: parseInt(userId) },
     });
     // 다음 이력서가 조회하질 않을 경우 없다고 반환하고
     if (!resume) {
@@ -93,10 +98,23 @@ router.post("/resumes/:userId", async (req, res, next) => {
         content,
       },
     });
-    return res.status(200).json({ message: { data: Updateresume } });
+    return res.status(200).json({ data: Updateresume });
   } catch (error) {
     next(error);
   }
+});
+
+// 이력서 삭제 API
+router.delete("/resumes/:resumeId", async (req, res, next) => {
+  const { resumeId } = req.params;
+
+  const resume = await prisma.resumes.findById(resumeId).exec();
+  if (!resume) {
+    return res.status(401).json({ message: "이력서 조회에 실패하였습니다." });
+  }
+  await resume.deleteOne({ _id: resumeId }).exec();
+
+  return res.status(200).json({ message: "이력서 삭제 완료!" });
 });
 
 export default router;
