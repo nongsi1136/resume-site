01e89d560a704199122ebf46ac2b526ddb39be13
sinhong jeong <jsh719803@gmail.com>
2024-02-02 14:53:56 +0900

    create Resume Update API

--- routers/documents.router.js
+++ routers/documents.router.js
@@ -68,4 +68,35 @@ router.get("/resumes/:userId", async (req, res, next) => {
   }
 });
 
+//이력서 수정 API 비즈니스 로직
+router.post("/resumes/:userId", async (req, res, next) => {
+  // 해당하는 이력서를 조회해야 겠징
+  try {
+    const resume = await prisma.resumes.findUnique({
+      where: { userId: parseInt(userId) },
+    });
+    // 다음 이력서가 조회하질 않을 경우 없다고 반환하고
+    if (!resume) {
+      return res.status(404).json({ message: "이력서 조회에 실패하였습니다." });
+    }
+    // 본인이 작성한 이력서에 대해서만 수정이 가능하게
+    if (resume.userId !== parseInt(userId)) {
+      return res
+        .status(401)
+        .json({ message: "해당 이력서를 수정할 권한이 없습니다" });
+    }
+    // 이력서 수정하기
+    const Updateresume = await prisma.resumes.update({
+      wherte: { userId: parseInt(userId) },
+      data: {
+        title,
+        content,
+      },
+    });
+    return res.status(200).json({ message: { data: Updateresume } });
+  } catch (error) {
+    next(error);
+  }
+});
+
 export default router;
