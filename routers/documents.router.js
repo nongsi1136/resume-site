7f5510b97d1b53ed60d6f7d4305e6acc6fdff0e3
sinhong jeong <jsh719803@gmail.com>
2024-02-02 13:14:36 +0900

    Added resume status to resume creation API.

--- routers/documents.router.js
+++ routers/documents.router.js
@@ -1,9 +1,31 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import authMiddleware from "../middlewares/auth.middleware.js";
+
 const router = express.Router();
 const prisma = new PrismaClient();
 
-router.get("/resumes", async (req, res) => {
+// 이력서 생성 API
+router.post("/resumes", async (req, res, next) => {
+  const { title, content, status = "APPLY" } = req.body;
+  const { userId } = req.user;
+  try {
+    const resume = await prisma.resumes.create({
+      data: {
+        userId: +userId,
+        title: title,
+        content: content,
+        status: status,
+      },
+    });
+    return res.status(201).json({ data: resume });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 모든 이력서 목록 조회 API 비즈니스 로직
+router.get("/resumes", async (req, res, next) => {
   const { orderKey, orderValue } = req.query;
 
   const resumes = await prisma.resumes.findMany({
@@ -21,11 +43,12 @@ router.get("/resumes", async (req, res) => {
   });
 });
 
-router.get("/resumes/:id", async (req, res, next) => {
-  const { id } = req.params;
+//이력서 상세 조회
+router.get("/resumes/:userId", async (req, res, next) => {
+  const { userId } = req.params;
 
   const resume = await prisma.resumes.findUnique({
-    where: { resumeId: parseInt(id) },
+    where: { resumeId: parseInt(userId) },
     select: {
       resumeId: true,
       title: true,
@@ -39,7 +62,7 @@ router.get("/resumes/:id", async (req, res, next) => {
   });
 
   if (!resume) {
-    return res.status(404).json({ errormessage: "이력서를 찾을 수 없습니다." });
+    return res.status(404).json({ message: "이력서를 찾을 수 없습니다." });
   }
 });
 
